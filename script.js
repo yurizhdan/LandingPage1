8ebe6cefe29e43d33247d6508f648c940f6a85b8
yurizhdan <yuri.zhdan@gmail.com>
2019-02-09 15:42:18 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$('#features').click(function () {
+    $('html').animate({
+        scrollTop: 810
+    }, 1000, 'easeInQuad')
+})
+
+$('#works').click(function () {
+    $('html').animate({
+        scrollTop: 1510
+    }, 1000, 'easeInQuad')
+})
+$('#team').click(function () {
+    $('html').animate({
+        scrollTop: 2210
+    }, 1000, 'easeInQuad')
+})
+$('#testimonials').click(function () {
+    $('html').animate({
+        scrollTop: 2910
+    }, 1000, 'easeInQuad')
+})
+$('#download').click(function () {
+    $('html').animate({
+        scrollTop: 3510
+    }, 1000, 'easeInQuad')
+})
